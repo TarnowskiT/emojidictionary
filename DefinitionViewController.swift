0346a9684395cf4ca693f3c55f7bba052b0c1d62
Trevor Tarnowski <ttarnowski@att.net>
2017-03-30 15:07:49 -0400

    added view controller class / definition view controller

---
+++ DefinitionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Trevor Tarnowski on 3/30/17.
+//  Copyright © 2017 Trevor Tarnowski. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
